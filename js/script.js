fed88eaa1961d3fb59f501acc5ff35a08c92d9a0
davidkiama <davidkiama98@gmail.com>
2021-12-19 22:03:27 +0300

    Added the hover effect with JQuery

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 "use strict";
 document.querySelector;
 $(document).ready(function () {
+  //Display and hide offer text
   $(".offer").click(function (e) {
     const offer = e.currentTarget;
 
@@ -11,4 +12,24 @@ $(document).ready(function () {
     icon.classList.toggle("hidden");
     text.classList.toggle("hidden");
   });
+
+  //Hover effect on the portfolio
+  $(".project").hover(
+    (e) => {
+      const project = e.currentTarget;
+      const projectImg = project.querySelector(".project__img");
+      const projectCaption = project.querySelector(".project__title");
+
+      projectImg.classList.add("dull");
+      projectCaption.classList.add("displayCaption");
+    },
+    (e) => {
+      const project = e.currentTarget;
+      const projectImg = project.querySelector(".project__img");
+      const projectCaption = project.querySelector(".project__title");
+
+      projectImg.classList.remove("dull");
+      projectCaption.classList.remove("displayCaption");
+    }
+  );
 });
