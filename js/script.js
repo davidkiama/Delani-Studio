9632dcd1baf2f444e1214d86a44a2f681eb4ffa5
davidkiama <davidkiama98@gmail.com>
2021-12-19 21:27:24 +0300

    Added event listerner for the offers

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 "use strict";
+document.querySelector;
+$(document).ready(function () {
+  $(".offer").click(function (e) {
+    const offer = e.currentTarget;
+
+    const icon = offer.querySelector(".offer__icon");
+    const text = offer.querySelector(".offer__text");
+
+    //add and remove hidden class
+    icon.classList.toggle("hidden");
+    text.classList.toggle("hidden");
+  });
+});
